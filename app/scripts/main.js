55102138c887c77eb261bce40b0cab21ce2e38c2
Cem <cemarguvanli@gmail.com>
2016-07-08 04:26:58 +0300

    add nav

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,1 +1,20 @@
-console.log('\'Allo \'Allo!');
+$(document).ready(function() {
+  $('.carousel').carousel({
+    interval: 5000 //changes the speed
+  });
+  var menu = $('#home nav');
+  var origOffsetY = menu.offset().top;
+
+  function scroll() {
+    if ($(window).scrollTop() >= origOffsetY) {
+      $('#home nav').addClass('sticky');
+    } else {
+      $('#home nav').removeClass('sticky');
+    }
+
+
+  }
+
+  document.onscroll = scroll;
+
+});
