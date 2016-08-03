4725227593d51c3d423d1c06ea469ae6e45db721
Cem <cemarguvanli@gmail.com>
2016-08-03 03:46:51 +0300

    refactoring

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,20 +1,83 @@
+'use strict';
+var config = {
+  elems: {}
+};
+
 $(document).ready(function() {
   $('.carousel').carousel({
-    interval: 5000 //changes the speed
+    interval: false //changes the speed
   });
+
+  // Sticky
   var menu = $('#home nav');
-  var origOffsetY = menu.offset().top;
+  var menuOrigOffsetY = menu.offset().top;
 
   function scroll() {
-    if ($(window).scrollTop() >= origOffsetY) {
+    if ($(window).scrollTop() >= menuOrigOffsetY) {
       $('#home nav').addClass('sticky');
     } else {
       $('#home nav').removeClass('sticky');
     }
-
-
   }
-
   document.onscroll = scroll;
 
+  // Isotope/Masonry
+  var $grid = $('.grid').imagesLoaded(function() {
+    // init Isotope after all images have loaded
+    $('.grid').isotope({
+      itemSelector: '.grid-item',
+      layoutMode: 'masonry',
+      masonry: {
+        columnWidth: 5
+      }
+    });
+    $('.works-filter').on('click', 'a', function(e) {
+      e.preventDefault();
+
+      $('.works-filter a').removeClass('active');
+      $(this).addClass('active');
+
+      var filterValue = $(this).attr('data-filter');
+
+      $grid.isotope({
+        filter: filterValue
+      });
+    });
+  });
 });
+
+// Nav Active
+var navActiveActions = function() {
+  var sections = $('section'),
+    nav = $('nav'),
+    navHeight = nav.outerHeight();
+
+  $(window).on('scroll', function() {
+    var curPos = $(this).scrollTop();
+    sections.each(function() {
+      var top = $(this).offset().top - navHeight,
+        bottom = top + $(this).outerHeight();
+
+      if (curPos >= top && curPos <= bottom) {
+        nav.find('a').removeClass('active');
+        sections.removeClass('active');
+
+        $(this).addClass('active');
+        nav.find('a[href="#' + $(this).attr('id') + '"]').addClass('active');
+      }
+    });
+  });
+
+  nav.find('a').on('click', function() {
+    var $el = $(this),
+      id = $el.attr('href');
+
+    $('html, body').animate({
+      scrollTop: $(id).offset().top - navHeight
+    }, 500);
+    $('.navbar-collapse').removeClass('in');
+    return false;
+  });
+};
+
+navActiveActions();
